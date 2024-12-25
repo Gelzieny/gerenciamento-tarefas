2738826a238fec2607d7262e130ac4f0387aa446
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-24 22:55:39 -0300

    Flask Create

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-
 tasks = []
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  data = request.get_json()
+  print(data)
+  return 'teste'
+
 
 if __name__ == "__main__":
   app.run(debug=True)
--- src/models/task.py
+++ src/models/task.py
@@ -1,4 +1,14 @@
 class Task:
   def __init__(self, id, title, description, completed=False) -> None:
-    sel
-    pass
+    self.id = id
+    self.title = title
+    self.description = description
+    self.completed = completed
+
+  def to_dict(self):
+    return{
+      "id": self.id,
+      "title": self.title,
+      "description": self.description,
+      "completed": self.completed
+    }
